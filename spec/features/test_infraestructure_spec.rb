7ad41fd0008b724d0234d4ef8af239788a5c812d
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2018-03-09 11:23:25 +0000

    Add HP for players

--- spec/features/test_infraestructure_spec.rb
+++ spec/features/test_infraestructure_spec.rb
@@ -6,12 +6,13 @@ describe Battle do
     visit '/'
   end
 
-  feature 'home' do
-
+  feature 'Homepage' do
     scenario 'checks if hompage says Welcome to the Iron Fist Tournament!' do
       expect(page).to have_content 'Welcome to the Iron Fist Tournament!'
     end
+  end
 
+  feature 'Names getting' do
     scenario 'checks if users can introduce their names' do
       visit '/players'
       fill_in 'name1', with: 'Hwoarang'
@@ -19,7 +20,23 @@ describe Battle do
       click_button 'Fighters selected'
       expect(page).to have_content "------------------------------ Hwoarang VS Jinpachi ------------------------------"
     end
+  end
 
+  feature 'View HP' do
+    scenario 'see Player 2 hit points' do
+      visit '/players'
+      fill_in 'name1', with: 'Hwoarang'
+      fill_in 'name2', with: 'Jinpachi'
+      click_button 'Fighters selected'
+      expect(page).to have_content "Jinpachi: 60HP"
+    end
+    scenario 'see Player 1 hit points' do
+      visit '/players'
+      fill_in 'name1', with: 'Hwoarang'
+      fill_in 'name2', with: 'Jinpachi'
+      click_button 'Fighters selected'
+      expect(page).to have_content "Hwoarang: 60HP"
+    end
   end
 
 end
